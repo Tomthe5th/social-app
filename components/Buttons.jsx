845fae5b73d2272a056e7e238aca4a9bdedc3ec7
Tom 「季林ユミル」 <swanhtet304y56@gmail.com>
2023-10-10 17:02:56 -0700

    #

---
+++ components/Buttons.jsx
@@ -0,0 +1,34 @@
+"use client";
+//move
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export function SignInButton() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    //when loading login form
+    return <p>...</p>;
+  }
+
+  if (status === "authenticated") {//if already logged in
+    return ( <Link href={"/dashboard"}>
+      <Image
+        src={session.user?.image}
+        alt={session.user.name}
+        width={32}
+        height={32}
+      />
+    </Link>)
+   
+  }
+  if (status === "unauthenticated") {
+    return <button onClick={() => signIn()}>Sign In</button>;
+  }
+}
+
+export function SignOutButton() {
+  return <button onClick={() => signOut()}>Sign Out</button>;
+}
